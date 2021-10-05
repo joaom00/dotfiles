8ff6f0c4ce686326fa76dcf61408dbc095a83fde
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-10-05 11:19:18 -0300

    refactor(gitsigns): add more options

--- .config/nvim/lua/jm/gitsigns.lua
+++ .config/nvim/lua/jm/gitsigns.lua
@@ -1,13 +1,13 @@
-local status_ok, gitsigns = pcall(require, "gitsigns")
-if not status_ok then
-  JM.notify "Missing gitsigns dependency"
-  return
-end
-
 local M = {}
 
 function M.config()
-  gitsigns.setup = {
+  local status_ok, gitsigns = pcall(require, "gitsigns")
+  if not status_ok then
+    JM.notify "Missing gitsigns dependency"
+    return
+  end
+
+  gitsigns.setup {
     signs = {
       add = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
       change = { hl = "GitSignsChange", text = "▍", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
@@ -15,6 +15,16 @@ function M.config()
       topdelete = { hl = "GitSignsDelete", text = "▾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
       changedelete = { hl = "GitSignsChange", text = "▍", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
     },
+    numhl = false,
+    linehl = false,
+    keymaps = {
+      noremap = true,
+      buffer = true,
+    },
+    watch_index = { interval = 1000 },
+    sign_priority = 6,
+    update_debounce = 200,
+    status_formatter = nil,
   }
 end
 
